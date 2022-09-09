45ed5758db0f0d6d46c493ce518a2ad1a646c719
sadique-cws <sadique.hussain96@gmail.com>
2022-09-09 16:03:04 +0530

    gender & nationality api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [genderDetails, setGenderDetails] = useState([]);
+  const [nationalityDetails, setNationalityDetails] = useState([]);
+  const [searchdata, setSearchData]  = useState("");
+  const GENDER_API = `https://api.genderize.io/?name=${searchdata}`
+  const NATIONALITY_API = `https://api.nationalize.io/?name=${searchdata}`
+
+
+  const handleSubmit = () => {
+    fetch(GENDER_API).then(response => response.json()).then(response => setGenderDetails(response))
+    fetch(NATIONALITY_API).then(response => response.json()).then(response => setNationalityDetails(response))
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <div className='w-1/3'>
+          <div className='bg-white rounded flex p-4'>
+              <input type="text" onChange={(e) => setSearchData(e.target.value)} value={searchdata} className="w-full border p-3 placeholder:text-black text-black" placeholder='Enter your name'/>
+              <input type="submit" className="bg-blue-500 text-white p-3" onClick={() => handleSubmit()}/>
+          </div>
+
+        {genderDetails.length != 0 && <div className='table  w-full mt-4' >
+          <table className='border-collapse border w-full'>
+              <tr className='border'>
+                <th className='border'>Name</th>
+                <td className='border'>{searchdata}</td>
+              </tr>
+              <tr className='border'>
+                <th className='border'>Nationality</th>
+                <td className='border'>{nationalityDetails.country.country_id}</td>
+              </tr>
+              <tr className='border'>
+                <th className='border'>Gender</th>
+                <td className='border'>{genderDetails.gender}</td>
+              </tr>
+              <tr className='border'>
+                <th className='border'>Name</th>
+                <td className='border'>sadique</td>
+              </tr>
+          </table>
+        </div>}
+
+          </div>
       </header>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/*/"],
+  content: ["src/*/**"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
